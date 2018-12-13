6284b22a434be1a2889846bc42d01617c44af075
Emilie H.B.S.B Madsbjerg <EmilieMadsbjerg@laptop-3cd4rvdc.kea.local>
2018-12-13 13:45:16 +0100

    final

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ window.addEventListener("load", showPage);
 
 function showPage() {
     console.log('Showing the page');
-    document.querySelector('.header_menu').addEventListener('click', removeHidden);
+    document.querySelector('.header_menu').addEventListener('click', showMenu);
 }
 
-function removeHidden() {
+function showMenu() {
     console.log('Remove hidden');
     document.querySelector('#burger_menu').classList.remove('hidden');
-    document.querySelector('.header_menu').removeEventListener('click', removeHidden);
-    document.querySelector('.header_menu').addEventListener('click', addHidden);
+    document.querySelector('.content').classList.add('hidden');
+    document.querySelector('.header_menu').removeEventListener('click', showMenu);
+    document.querySelector('.header_menu').addEventListener('click', hideMenu);
 }
 
-function addHidden() {
+function hideMenu() {
     console.log('Remove hidden');
     document.querySelector('#burger_menu').classList.add('hidden');
-    document.querySelector('.header_menu').removeEventListener('click', addHidden);
-    document.querySelector('.header_menu').addEventListener('click', removeHidden);
+    document.querySelector('.content').classList.remove('hidden');
+    document.querySelector('.header_menu').removeEventListener('click', hideMenu);
+    document.querySelector('.header_menu').addEventListener('click', showMenu);
 }
